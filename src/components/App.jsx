7237340787db8ef06d4cb4b9ed9b6779cec95faf
RaMonaPopa2023 <128323291+RaMonaPopa2023@users.noreply.github.com>
2023-12-07 00:02:14 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import ImageGallery from './ImageGallery';
 import ImageService from './ImageService';
 import ErrorAlert from './ErrorAlert';
@@ -15,52 +15,56 @@ const App = () => {
   const [perPage] = useState(20);
   const [totalHits, setTotalHits] = useState(0);
 
-  const triggerSearch = useCallback(
-    debounce(async () => {
+  const retrieveArticles = useCallback(
+    async (page = 1) => {
+      let uniqueArticles = [];
+      try {
+        setIsLoading(true);
+        const { hits, totalHits } = await ImageService.retrieveArticles(
+          searchTerm,
+          page,
+          perPage
+        );
+
+        if (hits.length === 0) {
+          setError('Nu a fost gasit rezultatul.');
+        } else {
+          uniqueArticles = hits.filter(article =>
+            articles.every(existingArticle => existingArticle.id !== article.id)
+          );
+        }
+
+        setArticles(prevArticles =>
+          page === 1 ? uniqueArticles : [...prevArticles, ...uniqueArticles]
+        );
+        setTotalHits(totalHits);
+      } catch (error) {
+        switch (error.code) {
+          case 'ERR_BAD_REQUEST':
+            setError('Nu a fost gasit rezultatul');
+            break;
+          default:
+            setError('A aparut o eroare.');
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [articles, searchTerm, perPage]
+  );
+
+  useEffect(() => {
+    const triggerSearch = debounce(async () => {
       setArticles([]);
       setError('');
       setIsLoading(true);
       setTotalHits(0);
       await retrieveArticles();
       setIsLoading(false);
-    }, 500),
-    [searchTerm]
-  );
+    }, 500);
 
-  const retrieveArticles = async (page = 1) => {
-    let uniqueArticles = [];
-    try {
-      setIsLoading(true);
-      const { hits, totalHits } = await ImageService.retrieveArticles(
-        searchTerm,
-        page,
-        perPage
-      );
-
-      if (hits.length === 0) {
-        setError('Nu a fost gasit rezultatul.');
-      } else {
-        uniqueArticles = hits.filter(article =>
-          articles.every(existingArticle => existingArticle.id !== article.id)
-        );
-      }
-
-      setArticles(prevArticles =>
-        page === 1 ? uniqueArticles : [...prevArticles, ...uniqueArticles]
-      );
-      setTotalHits(totalHits);
-    } catch (error) {
-      switch (error.code) {
-        case 'ERR_BAD_REQUEST':
-          setError('Nu a fost gasit rezultatul');
-          break;
-        default:
-          setError('A aparut o eroare.');
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  };
+    triggerSearch();
+  }, [searchTerm, retrieveArticles]);
 
   const loadMore = async () => {
     const nextPage = Math.ceil((visibleImages + perPage) / perPage);
